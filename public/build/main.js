c9300443ad77ea07c3a914c3267321aeedb221c8
Filip Ramstedt <filipramstedt@gmail.com>
2017-05-23 14:19:06 +0200

    built main.js

--- public/build/main.js
+++ public/build/main.js
@@ -27876,6 +27876,16 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
         _this.$store.dispatch('GET_POSTS');
       }
     });
+    if ("standalone" in window.navigator && window.navigator.standalone) {
+      // For iOS Apps
+      $('a').on('click', function (e) {
+        e.preventDefault();
+        var new_location = $(this).attr('href');
+        if (new_location != undefined && new_location.substr(0, 1) != '#' && $(this).attr('data-method') == undefined) {
+          window.location = new_location;
+        }
+      });
+    }
   },
   methods: {
     parseDate: function parseDate(date) {
